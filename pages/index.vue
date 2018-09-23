ce1989d82545ecf0dbfb6db85dfb92250e69b96d
Miami3 <olegkalysh25@gmail.com>
2018-09-23 23:51:00 +0300

    hero section markup

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,26 @@
 <template>
   <v-content>
-    <v-container fluid>
-      <v-layout>
-        <nav class="navigation">
-
-        </nav>
-        <v-layout class="hero__container">
-          <v-flex xs12 sm6>
+    <v-container fluid class="banner__container">
+      <v-layout align-center justify-center row fill-height>
+        <v-flex xs5>
+          <div class="logo">Atmosphonics</div>
+        </v-flex>
+        <v-flex>
+          <ul class="main__nav">
+            <li>How it works</li>
+            <li>Solutions</li>
+            <li>Features</li>
+          </ul>
+        </v-flex>
+        <v-flex>
+          <ul class="user__nav">
+            <li>Login</li>
+            <li class="user__nav--cta">Join Now</li>
+          </ul>
+        </v-flex>
+      </v-layout> <!-- Navigation -->
+      <v-layout class="hero__container">
+        <v-flex xs12 sm6>
             <h1 class="hero__title">
               We Help You to Get <br>
               Best Business Solutions Ever
@@ -16,12 +30,14 @@
               nesciunt omnis sequi sint!
             </p>
             <a href="#" class="hero__cta">Get Started Now</a>
-          </v-flex>
-          <v-flex xs12 sm6>
-            <img src="/home/macbook.png" alt="macbook" class="hero__img">
-          </v-flex>
-        </v-layout>
-      </v-layout>
+        </v-flex>
+        <v-flex xs12 sm6>
+          <div class="hero__img--wrapper">
+            <img src="/home/hero_img.jpeg" alt="team" class="hero__img">
+            <img src="/home/play_btn.png" alt="play  button" class="hero__play">
+          </div>
+        </v-flex>
+      </v-layout> <!-- Hero Container -->
     </v-container>
     <v-container fluid>
       footer
@@ -35,8 +51,51 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+
+  .banner__container {
+    background-color: #fff;
+    background-image: url('/home/home_banner.jpg');
+    background-position: top right;
+    background-size: contain;
+    background-repeat: no-repeat;
+  }
+
   .hero__img {
     width: 100%;
+  }
+
+  .main__nav, .user__nav {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    display: flex;
+    justify-content: center;
+
+    li {
+      display: inline-block;
+      margin: 0 10px;
+    }
+  }
+
+  .user__nav {
+    justify-content: flex-end;
+  }
+
+  .hero__img--wrapper {
+    position: relative;
+  }
+
+  .hero__play {
+    position: absolute;
+    z-index: 1;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    transition: all 0.5s ease;
+
+    &:hover {
+      cursor: pointer;
+    }
   }
 </style>
